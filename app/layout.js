e99887cfa55b52f23da0ee6e47d638ada90e6ed1
Muhammad <Muhammad124711@gmail.com>
2025-07-04 11:31:21 +0000

    analytics

--- app/layout.js
+++ app/layout.js
@@ -50,7 +50,7 @@ export default function RootLayout({ children }) {
         {/* Google Analytics Scripts */}
         <Script
           strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          src="https://www.googletagmanager.com/gtag/js?id=G-QZDD1NNBWS"
         />
         <Script
           id="gtag-init"
