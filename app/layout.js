752e452feae704391aaf947bdd83f1426fadd6cd
Muhammad <Muhammad124711@gmail.com>
2025-07-24 06:13:12 +0000

    add

--- app/layout.js
+++ app/layout.js
@@ -65,10 +65,12 @@ export default function RootLayout({ children }) {
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9928340261693986" crossorigin="anonymous"></script>
         
         <link rel="icon" href="/favicon.ico" sizes="48x48" />
-        {/* PropellerAds */}
+        {/* PropellerAds / monetag */}
         <meta name="monetag" content="28944bee1d3eea24347868a6a9cf8bb6"/>
-        {/* PropellerAds */}
+        {/* PropellerAds / monetag */}
         <script src="https://couphaithuph.net/act/files/tag.min.js?z=9579214" data-cfasync="false" async></script>
+        {/* PropellerAds / monetag */}
+        <script src="https://fpyf8.com/88/tag.min.js" data-zone="159065" async data-cfasync="false"></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -82,45 +84,6 @@ export default function RootLayout({ children }) {
             </ScreenSizeProvider>
           </ThemeProvider>
         </LanguageProvider>
-
-        {/* PopUpAds */}
-        <Script
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(){
-                var k=window,
-                s="e0f2982eed45d6c44c4f8c8d72066201",
-                r=[["siteId",957-904*112+5319675],["minBid",0.002],["popundersPerIP","2,2"],["delayBetween",300],["default",false],["defaultPerDay",5],["topmostLayer","auto"]],
-                e=["d3d3LmJldHRlcmFkc3lzdGVtLmNvbS90anNncmlkLm1pbi5jc3M=","ZDJrazBvM2ZyN2VkMDEuY2xvdWRmcm9udC5uZXQvWVVYL2hqc2ZpbGUubWluLmpz"],
-                l=-1,c,o,
-                h=function(){
-                  clearTimeout(o);
-                  l++;
-                  if(e[l] && !(1779094021000 < (new Date).getTime() && 1 < l)){
-                    c=k.document.createElement("script");
-                    c.type="text/javascript";
-                    c.async=!0;
-                    var t=k.document.getElementsByTagName("script")[0];
-                    c.src="https://"+atob(e[l]);
-                    c.crossOrigin="anonymous";
-                    c.onerror=h;
-                    c.onload=function(){
-                      clearTimeout(o);
-                      k[s.slice(0,16)+s.slice(0,16)] || h();
-                    };
-                    o=setTimeout(h,5000);
-                    t.parentNode.insertBefore(c,t);
-                  }
-                };
-                if(!k[s]){
-                  try{Object.freeze(k[s]=r)}catch(e){}
-                  h();
-                }
-              })();
-            `,
-          }}
-        />
       </body>
     </html>
   );
