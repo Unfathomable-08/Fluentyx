9dd5aa64652debbb0bbd500b431275c60b32d2ed
Muhammad <Muhammad124711@gmail.com>
2025-07-28 07:26:41 +0000

    chap 9 added

--- app/layout.js
+++ app/layout.js
@@ -50,6 +50,7 @@ export default function RootLayout({ children }) {
         {/* Shortcut */}
         <link rel="manifest" href="/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Flunetyx" />
         <link rel="apple-touch-icon" href="/fluentyx.png" />
         {/* Google Fonts */}
